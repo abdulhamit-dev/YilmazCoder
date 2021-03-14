7a77d04d6706d3700d9267ad36823967ae7cff3f
hamitylmz92 <hmd62392@gmail.com>
2021-03-14 13:32:39 +0300

    Kategori clasıı eklendi

---
+++ CoderBlog.Entities/Kategori.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoderBlog.Entities
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+    }
+}
