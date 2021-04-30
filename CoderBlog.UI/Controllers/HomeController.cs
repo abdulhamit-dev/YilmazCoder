017e6531a342924b288048e82a15fcfe26a6651a
hamitylmz92 <hmd62392@gmail.com>
2021-05-01 01:03:35 +0300

    UI Katmanı eklendi

---
+++ CoderBlog.UI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CoderBlog.Business.Concrete;
+using CoderBlog.Entities.Dtos;
+using CoderBlog.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderBlog.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        YaziManager yaziManager = new YaziManager();
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            List<YaziDto> yaziList = new List<YaziDto>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://api.yilmazcoder.online/api/yazi/getlistyeniler");
+                if (response != null)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    yaziList=  JsonConvert.DeserializeObject<List<YaziDto>>(jsonString);
+
+                }
+            }
+            return View(yaziList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoderBlog.UI/Controllers/YaziController.cs
@@ -0,0 +1,27 @@
+﻿using CoderBlog.Business.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderBlog.UI.Controllers
+{
+    public class YaziController : Controller
+    {
+        KategoriManager kategoriManager = new KategoriManager();
+        public IActionResult Yeni()
+        {
+            ViewBag.KategoriList = kategoriManager.GetList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostTest(string deger)
+        {
+
+            return Json(true);
+        }
+        
+    }
+}
